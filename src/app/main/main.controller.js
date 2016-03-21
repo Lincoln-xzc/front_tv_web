30c198f30b84bf8ef113377c6a9a9dd9674498fd
Lincoln-xzc <xzc524813657@sina.com>
2016-03-21 17:35:08 +0800

    update

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,34 +6,33 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController() {
     var vm = this;
+    vm.test = 'test';
+    vm.slides = [
+      {
+        image:'/assets/images/gulp.png',
+        text:'I love that',
+        id: 1
+      },
+      {
+        image:'/assets/images/karma.png',
+        text:'Nice image',
+        id: 2
+      },
+      {
+        image:'../assets/images/karma.png',
+        text:'Nice image',
+        id: 3
+      },
+      {
+        image:'../assets/images/yeoman.png',
+        text:'Nice image',
+        id: 4
+      }
+    ];
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1458447065398;
-    vm.showToastr = showToastr;
 
-    activate();
 
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
   }
 })();

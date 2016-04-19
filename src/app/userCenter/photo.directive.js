7837b39fc155cec533b7f190a53c457fd9836788
Lincoln-xzc <xzc524813657@sina.com>
2016-04-19 23:04:46 +0800

    'update'

--- src/app/userCenter/photo.directive.js
+++ src/app/userCenter/photo.directive.js
@@ -25,13 +25,30 @@
           if(delta > 0){
             angular.element(".cropper-viewer img").css('width',"-=11.742");
             angular.element(".cropper-viewer img").css('height',"-=11.742");
+
             angular.element(".cropper-container img").eq(0).css("width","-=11.742");
             angular.element(".cropper-container img").eq(0).css("height","-=11.742");
+            angular.element(".cropper-container img").eq(0).css("margin-top","+=10.742");
+            angular.element(".cropper-container img").eq(0).css("margin-left","+=10.742");
+            angular.element(".preview-lg img").css('width',"-=9.742");
+            angular.element('.preview-lg img').css('height','-=9.742');
+            angular.element('.preview-md img').css('width','-=7.742');
+            angular.element('.preview-md img').css('height','-=7.742');
+            angular.element('.preview-sm img').css('width','-=5.742');
+            angular.element('.preview-sm img').css('height','-=5.742');
           } else if(delta <0){
             angular.element(".cropper-viewer img").css('width',"+=11.742");
             angular.element(".cropper-viewer img").css('height',"+=11.742");
             angular.element(".cropper-container img").eq(0).css("width","+=11.742");
             angular.element(".cropper-container img").eq(0).css("height","+=11.742");
+            angular.element(".cropper-container img").eq(0).css("margin-top","-=10.742");
+            angular.element(".cropper-container img").eq(0).css("margin-left","-=10.742");
+            angular.element('.preview-lg img').css('width','+=9.742');
+            angular.element('.preview-lg img').css('height','+=9.742');
+            angular.element('.preview-md img').css('width','+=7.742');
+            angular.element('.preview-md img').css('height','+=7.742');
+            angular.element('.preview-sm img').css('width','+=5.742');
+            angular.element('.preview-sm img').css('height','+=5.742');
           }
           event.preventDefault();
         });
